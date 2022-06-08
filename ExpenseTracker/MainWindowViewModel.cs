da996e005ad6f1a9633c3f078e73cc46c27a2319
jaentuna <johnalson_entuna@outlook.com>
2022-06-08 11:03:35 +0800

    - implementing more things - adding more style resources - adding views

--- ExpenseTracker/MainWindowViewModel.cs
+++ ExpenseTracker/MainWindowViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using ExpenseTracker.Data;
 using ExpenseTracker.View;
-using System.Diagnostics;
 using System.Windows.Input;
 using WPFWrappers;
 using WPFWrappers.Command;
@@ -16,6 +15,7 @@ namespace ExpenseTracker
             set => SetProperty(ref _currentVarExpense, value);
         }
         public ICommand CreateVariableExpenseCommand => new RelayCommand(f => { CreateVariableExpense(); }, f => true);
+        public ICommand OpenVariableExpenseCommand => new RelayCommand(f => { OpenVariableExpense(); }, f => true);
         public MainWindowViewModel()
         {
             // Load the data...
@@ -24,11 +24,16 @@ namespace ExpenseTracker
 
         private void CreateVariableExpense()
         {
-            CreateVariableExpenseWindow window = new CreateVariableExpenseWindow();
+            CreateVariableExpenseWindow window = new();
             if (window.ShowDialog() ?? true)
             {
                 CurrentVarExpense = window.Expense;
             }
+        }
+
+        private void OpenVariableExpense()
+        {
+
         }
     }
 }
---
+++ ExpenseTracker/ViewModels/VariableExpenseViewModel.cs
@@ -0,0 +1,24 @@
+﻿using ExpenseTracker.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFWrappers;
+
+namespace ExpenseTracker.ViewModels
+{
+    class VariableExpenseViewModel : ViewModel
+    {
+        private VariableExpense _expense;
+        public VariableExpense Expense
+        {
+            get => _expense;
+            set => SetProperty(ref _expense, value);
+        }
+        public VariableExpenseViewModel()
+        {
+
+        }
+    }
+}
