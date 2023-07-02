78e13f33f4a22fbde7165b3872e48f3e39f9d97c
AlsonEntuna <alson.entuna@outlook.com>
2023-07-02 22:45:39 +0200

    committing progress to the cache system whenever api call is null

--- ExpenseTracker/Data/CurrencyConverter.cs
+++ ExpenseTracker/Data/CurrencyConverter.cs
@@ -9,9 +9,38 @@ using System.IO;
 
 namespace ExpenseTracker.Data
 {
+    public class ConversionData
+    {
+        public string Key;
+        public float Value;
+        public ConversionData() { }
+        public override bool Equals(object obj)
+        {
+            if (obj is ConversionData other)
+                return string.Equals(Key, other.Key);
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Key.GetHashCode();
+        }
+    }
     public class CurrencyConverter
     {
-        public CurrencyConverter() { }
+        private string _cachePath = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "_cache", "currency_conversion.json");
+        private List<ConversionData> _cachedConversionData = new List<ConversionData>();
+        public CurrencyConverter() 
+        {
+            if (!File.Exists(_cachePath))
+            {
+                JsonUtils.SerializeArray(_cachePath, _cachedConversionData);
+            }
+            else
+            {
+                _cachedConversionData = JsonUtils.DeserializeArray<List<ConversionData>>(_cachePath);
+            }
+        }
 
         static async Task<float> GetCurrencyConversion(string from, string to)
         {
@@ -24,22 +53,54 @@ namespace ExpenseTracker.Data
             var conversionData = JsonSerializer.DeserializeAsync<Dictionary<string, float>>(stream);
 
             conversionData.Result.TryGetValue(conversionKey, out float val);
+            
             return val;
+        }
+
+        private ConversionData GetConversionData(string key) 
+        {
+            foreach (var conversionData in _cachedConversionData)
+            {
+                if (string.Equals(conversionData.Key, key))
+                {
+                    return conversionData;
+                }
+            }
+            return null;
         }
 
         public async void Convert(DataEntry entry, string toCurrency)
         {
+            float conversionRate = 1;
+            string conversionKey = $"{toCurrency}_{entry.Currency.Code}";
             try
             {
-                float conversionRate = await GetCurrencyConversion(entry.Currency.Code, toCurrency);
-                entry.Amount = (float)Math.Round(entry.Amount / conversionRate, 2);
+                conversionRate = await GetCurrencyConversion(entry.Currency.Code, toCurrency);
+
+                // Add to the cache
+                ConversionData cachedData = new ConversionData()
+                {
+                    Key = conversionKey,
+                    Value = conversionRate
+                };
+                if (!_cachedConversionData.Contains(cachedData))
+                    _cachedConversionData.Add(cachedData);
+                else
+                {
+                    var data = GetConversionData(cachedData.Key);
+                    if (data != null)
+                        data.Value = cachedData.Value;
+                }
+                JsonUtils.SerializeArray(_cachePath, _cachedConversionData);
             }
             catch
             {
                 // TODO: make a cache system for this purpose...
-                return;
+                var data = GetConversionData(conversionKey);
+                if (data != null)
+                    conversionRate = data.Value;
             }
-            
+            entry.Amount = (float)Math.Round(entry.Amount / conversionRate, 2);
         }
     }
 }
