0383957916f956097dbace81a18287aecc3ceb9f
AlsonEntuna <alson.entuna@outlook.com>
2023-08-01 23:27:59 +0200

    changing to the new api key

--- ExpenseTracker/ExpenseSys/Constants.cs
+++ ExpenseTracker/ExpenseSys/Constants.cs
@@ -6,6 +6,6 @@
         public static string CONFIG_FILE => "config.cfg";
         public static string CATEGORIES_FILE => "categories.json";
         public static string DEFAULT_EXPENSE_FILENAME = "Expense.exp";
-        public static string API_KEY => "058a9b7899f9d7fe22bf";
+        public static string API_KEY => "63216d7f4275580b6660";
     }
 }
