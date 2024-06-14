72a29cd90d126cdd99f07a845f388e3a6e8c60d7
Alson Entuna <alson.entuna@outlook.com>
2024-06-14 21:40:18 +0200

    * updating API key for Juni 2024

--- ExpenseTracker.CurrencyConverter/Config/Keys.cs
+++ ExpenseTracker.CurrencyConverter/Config/Keys.cs
@@ -2,6 +2,6 @@
 {
     internal static class Keys
     {
-        public static string CURRENCY_CONVERTER_API_KEY = "c96af9bb812d9825cba8";
+        public static string CURRENCY_CONVERTER_API_KEY = "ccbaaff09b30c5dca2bf";
     }
 }
