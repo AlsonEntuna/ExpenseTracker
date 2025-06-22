4892d39df7e302de399d105ac1d3924dc21cb794
ANGELICA-LEGION\angee <alson.entuna@outlook.com>
2025-06-22 21:07:11 +0200

    * changes to the listutils

--- ExpenseTracker.Tools/ListUtils.cs
+++ ExpenseTracker.Tools/ListUtils.cs
@@ -6,12 +6,8 @@ namespace ExpenseTracker.Tools
     {
         public static ObservableCollection<T> ToObservableCollection<T>(IEnumerable<T> list)
         {
-            ObservableCollection<T> newList = new ObservableCollection<T>();
-            foreach (T val in list)
-            {
-                newList.Add(val);
-            }
-            return newList;
+            ObservableCollection<T> collection = [.. list];
+            return collection;
         }
     }
 }
