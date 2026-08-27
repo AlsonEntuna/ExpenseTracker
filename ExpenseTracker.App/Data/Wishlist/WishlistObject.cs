7dc8a1ee0c227eea8d1beb2f056f4fc9ae6988fa
Alson Entuna <alson.entuna@outlook.com>
2026-08-27 20:26:29 +0200

    * wip

---
+++ ExpenseTracker.App/Data/Wishlist/WishlistObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Data.Wishlist
+{
+    internal class WishlistObject
+    {
+        public enum WishlistState
+        {
+            NotStarted,
+            InProgress,
+            OnHold,
+            Completed,
+            _COUNT
+        }
+
+        private WishlistState _state;
+    }
+}
---
+++ ExpenseTracker.App/View/Wishlist/WishlistObjectViewModel.cs
@@ -0,0 +1,21 @@
+﻿using ExpenseTracker.Data;
+using ExpenseTracker.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.View.Wishlist
+{    
+    internal class WishlistObjectViewModel : ViewModel
+    {
+        
+
+        public WishlistObjectViewModel()
+        {
+            // Register to the app instance connection
+            AppInstance.Connection.AddViewModel(this);
+        }
+    }
+}
